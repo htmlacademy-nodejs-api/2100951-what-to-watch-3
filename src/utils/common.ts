dd05c3a188bda9bdd204fa24212e0ee1b2961095
Mary0708 <mary.liss133@gmail.com>
2023-02-14 16:53:11 +0500

    exception filter

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -6,6 +6,7 @@ import { FilmType } from '../types/films.type.js';
 import { GenresType } from '../types/genres-type.enum.js';
 import { ValidationError } from 'class-validator';
 import { ValidationErrorField } from '../types/validation-error-field.type.js';
+import { ServiceError } from '../types/service-error.enum.js';
 
 export const createFilm = (row: string) => {
   const tokens = row.replace('\n', '').split('\t');
@@ -60,8 +61,10 @@ export const createSHA256 = (line: string, salt: string): string => {
 export const fillDTO = <T, V>(someDto: ClassConstructor<T>, plainObject: V) =>
   plainToInstance(someDto, plainObject, { excludeExtraneousValues: true });
 
-export const createErrorObject = (message: string) => ({
-  error: message,
+export const createErrorObject = (serviceError: ServiceError, message: string, details: ValidationErrorField[] = []) => ({
+  errorType: serviceError,
+  message,
+  details: [...details]
 });
 
 export const createJWT = async (algoritm: string, jwtSecret: string, payload: object): Promise<string> =>
